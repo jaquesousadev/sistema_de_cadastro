b2b3e37246f27c9a4698d8894f488c22919780b9
Jaque Sousa <jaquelinesousaf@gmail.com>
2024-12-30 16:09:36 -0300

    alterações para subir na locaweb

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,17 @@
+require('dotenv').config(); // Carrega variáveis de ambiente do arquivo .env
+
 const mysql = require('mysql');
-require('dotenv').config();
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
   if (err) {
-    console.error('Erro ao conectar ao banco de dados: ', err);
+    console.error('Erro ao conectar ao banco de dados:', err);
     return;
   }
   console.log('Conectado ao banco de dados MySQL');
